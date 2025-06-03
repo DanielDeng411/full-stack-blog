16bac7cb23fc314d21194ef5e6bd8a1e9dcfc1cc
DanielDeng411 <DanielDeng1522@gmail.com>
2025-06-03 19:51:17 -0400

    Create blog app

---
+++ src/components/Image.jsx
@@ -0,0 +1,18 @@
+import { Image } from '@imagekit/react';
+
+const IKImage = ({ src, className, w, h, alt }) => {
+    return (
+        <Image
+            urlEndpoint="https://ik.imagekit.io/ojy4zls8n/"
+            src={src}
+            className={className}
+            loading="lazy"
+            alt={alt}
+            width={w}
+            height={h}
+            lqip={{ active: true, quality: 20 }}
+        />
+    );
+};
+
+export default IKImage;
